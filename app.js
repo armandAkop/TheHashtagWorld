d9c31f95f73e24e724939d509b759823f023dfb8
Armand Akopian <armand.akopyan@gmail.com>
2015-07-07 21:04:49 -0700

    Removed users routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var home = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
