11c44bb04213610e56d2d491fc216828b6f467c6
Armand Akopian <armand.akopyan@gmail.com>
2015-08-01 10:52:29 -0700

    Changed routing to use app.get instead of app.set

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', home);
+app.get('/', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, callback) {
