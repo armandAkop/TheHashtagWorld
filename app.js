5961cdfa226ec2373ff25813626146fa498be6c9
Armand Akopian <armand.akopyan@gmail.com>
2015-09-08 18:09:33 -0700

    Added GZIP compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compress = require('compression');
 
 var homepageRoutes = require('./lib/routes/index');
 
@@ -29,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compress());
 
 app.use('/', homepageRoutes);
 
